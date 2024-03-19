7158a6a953adcef7744abd5b4d37ec86ee1d81a1
harifa123 <harifarifu@gmail.com>
2024-03-19 22:11:01 +0530

    event model and router for adding event and viewing events

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const cors=require("cors")
 
 const app=express()
 const port =8085;
-
+const eventRouter = require("./controllers/eventRouter")
 app.use(express.json())
 
+
+app.use("/api/events", eventRouter)
 
 
 app.listen(port, ()=>{
