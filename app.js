6d55051cf61818658a9fe00805fc913d08e26771
jeff1999git <jeffinmjtcr@gmail.com>
2024-03-19 16:59:08 +0530

    folders created

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express=require("express")
 const cors=require("cors")
 
 const app=express()
+const port =8085;
 
-app.listen(8085)
+app.use(express.json())
+
+
+
+app.listen(port, ()=>{
+    console.log("Server running on",port)
+})
