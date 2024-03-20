c7b9b3d4794367167d24a564990cf07936b61e35
Annateresa01 <shajianna013@gmail.com>
2024-03-20 17:17:41 +0530

    ok

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const adminRouter=require('./controllers/adminRouter')
 
 const studentRouter=require('./controllers/studentRouter')
 
-const feedbackRouter=require("./controllers/feedbackRouter")
+//const feedbackRouter=require("./controllers/feedbackRouter")
 
 
 
 const app=express()
 const port =8085;
-const eventRouter = require("./controllers/eventRouter")
+//const eventRouter = require("./controllers/eventRouter")
 app.use(express.json())
 app.use(cors())
 
@@ -26,7 +26,7 @@ app.use("/api/college",collegeRouter)
 
 app.use("/api/payment",paymentRouter)
 
-app.use("/api/events", eventRouter)
+//app.use("/api/events", eventRouter)
 
 app.use("/api/admin",adminRouter)
 
@@ -35,7 +35,7 @@ app.use("/api/student",studentRouter)
 
 app.use("/api/users",userRouter)
 
-app.use("/api/feedback", feedbackRouter)
+//app.use("/api/feedback", feedbackRouter)
 
 
 
