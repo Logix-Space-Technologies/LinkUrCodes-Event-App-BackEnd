2fc1df5d6972d445ac8618eb22f62ba8018c1a78
Mahadevan1234 <mahi17700@gmail.com>
2024-03-20 11:27:29 +0530

    student model created

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const paymentRouter=require("./controllers/paymentRouter")
 
 const adminRouter=require('./controllers/adminRouter')
 
+const studentRouter=require('./controllers/studentRouter')
+
 
 const app=express()
 const port =8085;
@@ -13,6 +15,8 @@ app.use(express.json())
 app.use("/api/payment",paymentRouter)
 
 app.use("/api/admin",adminRouter)
+
+app.use("/api/student",studentRouter)
 
 app.listen(port, ()=>{
     console.log("Server running on",port)
