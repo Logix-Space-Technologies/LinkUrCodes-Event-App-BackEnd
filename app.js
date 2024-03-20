06f209f58ddd48999b517ac4405ebbd1511b0aa4
Joyna-Joy <joynajoy24@gmail.com>
2024-03-20 10:31:52 +0530

    College Details

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express=require("express")
 const cors=require("cors")
+const collegeRouter=require("./controllers/collegeRouter")
 
 const app=express()
 const port =8085;
 
 app.use(express.json())
+app.use(cors())
 
 
+app.use("/api/college",collegeRouter)
 
 app.listen(port, ()=>{
     console.log("Server running on",port)
