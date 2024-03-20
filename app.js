bc3d8f094357d2495ce76229785130524be4195b
ArunKuriakose15 <arunkp1515@gmail.com>
2024-03-20 11:05:33 +0530

    feedback model

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const cors=require("cors")
+const feedbackRouter=require("./controllers/feedbackRouter")
 
 const app=express()
 const port =8085;
 
 app.use(express.json())
 
+app.use("/api/feedback", feedbackRouter)
 
 
 app.listen(port, ()=>{
