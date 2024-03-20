dd5512ea6e64673043bfe32bae52b36f877a413c
TaniyaMathew1 <taniyamathew2000@gmail.com>
2024-03-20 09:22:10 +0530

    admin model created

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express=require("express")
 const cors=require("cors")
+const adminRouter=require('./controllers/adminRouter')
 
 const app=express()
 const port =8085;
 
 app.use(express.json())
 
-
+app.use("/api/admin",adminRouter)
 
 app.listen(port, ()=>{
     console.log("Server running on",port)
