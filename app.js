cd54a2d0d935d9e70942d5395a3f6af0ae2d79b4
Annateresa01 <shajianna013@gmail.com>
2024-03-20 09:57:02 +0530

    ok

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const cors=require("cors")
+const paymentRouter=require("./controllers/paymentRouter")
 
 const app=express()
 const port =8085;
 
 app.use(express.json())
+app.use("/api/payment",paymentRouter)
 
 
 
---
+++ controllers/paymentRouter.js
@@ -0,0 +1,28 @@
+const express=require("express")
+const paymentModel=require("../models/paymentModel")
+const { error } = require("console");
+
+const router=express.Router()
+
+router.post('/addpayment', (req, res) => {
+    console.log(req.body)
+  paymentModel.insertpayment(req.body, (error, results) => {
+    if (error) {
+      res.status(500).send('Error in gaving payments'+error);
+      return;
+    }
+    res.status(201).send(`payment added with ID: ${results.insertId}`);
+  });
+});
+
+router.get('/viewpaymenthistory',(req,res)=>{
+    paymentModel.viewPayments((error,results)=>{
+      if(error){
+        res.status(500).send('Error fetching payments:'+error)
+        return
+      }
+      res.status(200).json(results);
+    })
+  })
+
+  module.exports=router;
