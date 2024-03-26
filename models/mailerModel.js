ac5bb7991bce66e1c284c8fe65635291829d07cc
Annateresa01 <shajianna013@gmail.com>
2024-03-26 14:54:20 +0530

    ok

--- models/mailerModel.js
+++ models/mailerModel.js
@@ -1,6 +1,41 @@
+// const nodemailer = require("nodemailer");
+
+// const sendPasswordResetEmail = async (student_email, student_name) => {
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false,
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: process.env.EMAIL_USER,
+//         to: student_email,
+//         subject: subjectheading,
+//         text: textsend
+//     };
+
+//     return new Promise((resolve, reject) => {
+//         transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//                 console.error('Error sending password reset email:', error);
+//                 reject('Failed to send password reset email');
+//             } else {
+//                 console.log('Password reset email sent:', info.response);
+//                 resolve('Password reset message has been sent to your email');
+//             }
+//         });
+//     });
+// };
+
+// module.exports = { sendPasswordResetEmail };
 const nodemailer = require("nodemailer");
 
-const sendPasswordResetEmail = async (student_email, student_name) => {
+const sendPasswordResetEmail = async (student_email, student_name, subjectheading, textsend) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -15,8 +50,8 @@ const sendPasswordResetEmail = async (student_email, student_name) => {
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: student_email,
-        subject: 'Password Reset',
-        text: `Dear ${student_name},\n\nYou have requested to reset your password. Please contact the administrator for assistance.`,
+        subject: subjectheading,
+        text: textsend
     };
 
     return new Promise((resolve, reject) => {
@@ -33,3 +68,4 @@ const sendPasswordResetEmail = async (student_email, student_name) => {
 };
 
 module.exports = { sendPasswordResetEmail };
+
