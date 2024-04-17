51d6412409d6b79fca9c75f432f526371d0be13f
ArunKuriakose15 <arunkp1515@gmail.com>
2024-04-17 10:34:10 +0530

    private event add,update,delete,retreive,view & public event view delete retreive

--- controllers/eventRouter.js
+++ controllers/eventRouter.js
@@ -120,22 +120,31 @@ router.post('/view_private_events', (req, res) => {
     });
 })
 
-router.put('/update_private_events', (req, res) => {
-    const { event_private_id, updatedFields } = req.body;
-
-    if (!event_private_id || !updatedFields) {
-        return res.status(400).json({ error: 'Event ID and updated fields are required' });
-    }
+router.post('/update_private_events', upload.single('image'),async (req, res) => {
+   
     const token=req.headers["token"]
    jwt.verify(token,"eventAdmin",(error,decoded)=>{
     if (decoded && decoded.adminUsername) {
-    privateEventModel.updatePrivateEvents(event_private_id, updatedFields, (error, result) => {
+        const imagePath = req.file.path; //image path
+        let data = req.body
+        let event_id= data.event_private_id
+        const newData = {
+            event_private_name: data.event_private_name,
+            event_private_amount: data.event_private_amount,
+            event_private_description: data.event_private_description,
+            event_private_date: data.event_private_date,
+            event_private_time: data.event_private_time,
+            event_private_image: imagePath,
+            event_updatedby: data.event_updatedby
+        }
+        console.log(newData)
+    privateEventModel.updatePrivateEvents(event_id,newData, (error, result) => {
         if (error) {
             console.error('Error updating event:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
+            res.status(500).json({ status: 'Error' });
         } else {
             console.log('Event updated successfully');
-            res.status(200).json({ message: 'Event updated successfully' });
+            res.status(200).json({ status: 'Event updated successfully' });
         }
       });
     }
@@ -220,6 +229,128 @@ router.post('/search-private-events', (req, res) => {
         })
     }
 })
+});
+
+router.post('/delete_private_event', async (req, res) => {
+    try {
+        const { event_private_id } = req.body;
+        const token = req.headers["token"]
+        jwt.verify(token, "eventAdmin", (error, decoded) => {
+            if (decoded && decoded.adminUsername) {
+                privateEventModel.deletePrivateEvent(event_private_id, (error, result) => {
+                    if (error) {
+                        return res.status(500).json({ status: 'error', error: 'Error deleting event' });
+                    }
+                    res.json({ status: 'success' });
+                });
+            }
+            else {
+                return res.json({ "status": "unauthorised user" });
+            }
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', error: 'An error occurred while deleting the college' });
+    }
+});
+
+router.post('/retrive_private_event', async (req, res) => {
+    try {
+        const { event_private_id } = req.body;
+        const token = req.headers["token"]
+        jwt.verify(token, "eventAdmin", (error, decoded) => {
+            if (decoded && decoded.adminUsername) {
+                privateEventModel.retrivePrivateEvent(event_private_id, (error, result) => {
+                    if (error) {
+                        return res.status(500).json({ status: 'error', error: 'Error deleting event' });
+                    }
+                    res.json({ status: 'success' });
+                });
+            }
+            else {
+                return res.json({ "status": "unauthorised user" });
+            }
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', error: 'An error occurred while deleting the college' });
+    }
+});
+
+router.post('/view_active_private_events', (req, res) => {
+    const admintoken = req.headers["token"];
+    jwt.verify(admintoken, "eventAdmin", async (error, decoded) => {
+        if (error) {
+            console.log({ "status": "error", "message": "Failed to verify token" })
+            return res.json({ "status": "unauthorised user" });
+        }
+        if (decoded && decoded.adminUsername) {
+            privateEventModel.viewActiveEvents((error, results) => {
+                res.json(results);
+            })
+        }
+    });
+})
+
+router.post('/view_deleted_private_events', (req, res) => {
+    const admintoken = req.headers["token"];
+    jwt.verify(admintoken, "eventAdmin", async (error, decoded) => {
+        if (error) {
+            console.log({ "status": "error", "message": "Failed to verify token" })
+            return res.json({ "status": "unauthorised user" });
+        }
+        if (decoded && decoded.adminUsername) {
+            privateEventModel.viewDeletedEvents((error, results) => {
+                res.json(results);
+            })
+        }
+    });
+})
+
+router.post('/delete_public_event', async (req, res) => {
+    try {
+        const { event_public_id } = req.body;
+        const token = req.headers["token"]
+        jwt.verify(token, "eventAdmin", (error, decoded) => {
+            if (decoded && decoded.adminUsername) {
+                publicEventModel.deletePublicEvent(event_public_id, (error, result) => {
+                    if (error) {
+                        return res.status(500).json({ status: 'error', error: 'Error deleting event' });
+                    }
+                    res.json({ status: 'success' });
+                });
+            }
+            else {
+                return res.json({ "status": "unauthorised user" });
+            }
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', error: 'An error occurred while deleting the college' });
+    }
+});
+
+router.post('/retrive_public_event', async (req, res) => {
+    try {
+        const { event_public_id } = req.body;
+        const token = req.headers["token"]
+        jwt.verify(token, "eventAdmin", (error, decoded) => {
+            if (decoded && decoded.adminUsername) {
+                publicEventModel.retrivePublicEvent(event_public_id, (error, result) => {
+                    if (error) {
+                        return res.status(500).json({ status: 'error', error: 'Error deleting event' });
+                    }
+                    res.json({ status: 'success' });
+                });
+            }
+            else {
+                return res.json({ "status": "unauthorised user" });
+            }
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', error: 'An error occurred while deleting the college' });
+    }
 });
 
 module.exports = router
