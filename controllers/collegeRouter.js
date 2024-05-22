ad90f6ea4531683cb22ee207dd9b21db98940792
Mahadevan1234 <mahi17700@gmail.com>
2024-05-22 20:11:00 +0530

    admin add college testing

--- controllers/collegeRouter.js
+++ controllers/collegeRouter.js
@@ -42,7 +42,8 @@ router.post('/addCollege', uploadModel.CollegeImageupload.single('image'), async
                         return;
                     }
 
-                    res.status(201).send('College added with ID: ' + results.insertId);
+                    //res.status(201).send('College added with ID: ' + results.insertId);
+                    res.json({status:"success"});
                 });
             }
             else {
