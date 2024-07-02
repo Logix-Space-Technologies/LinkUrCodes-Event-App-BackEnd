0c646e853d46aa09be1077c94b0f9fc365ebf1fe
ArunKuriakose15 <arunkp1515@gmail.com>
2024-07-02 19:55:25 +0530

    public event session feedback done

--- models/feedbackModel.js
+++ models/feedbackModel.js
@@ -30,13 +30,23 @@ const packageModel = {
         pool.query(query,[event_id], callback);
     },
 
-    // session feedback
+    // private session feedback 
     insertFeedbackSessionStud: (feedbackSessionStudData, callback) => {
         const query = 'INSERT INTO feedback_session_private SET ? ';
         pool.query(query, feedbackSessionStudData, callback);
     },
     viewFeedbackSessionStud: (sessionId, callback) => {
         const query = 'SELECT fs.feedback_id, s.student_name, sp.session_topic_description, sp.type, fs.feedback_contents, fs.addedby_date FROM feedback_session_private fs JOIN student s ON fs.student_id = s.student_id JOIN session_private sp ON fs.session_id = sp.session_private_id WHERE session_id = ?';
+        pool.query(query, [sessionId], callback);
+    },
+
+    //public session feedback
+    insertFeedbackSessionUser: (feedbackSessionUserData, callback) => {
+        const query = 'INSERT INTO feedback_session_public SET ? ';
+        pool.query(query, feedbackSessionUserData, callback);
+    },
+    viewFeedbackSessionUser: (sessionId, callback) => {
+        const query = 'SELECT fs.feedback_id, u.user_name, fs.feedback_contents, fs.addedby_date FROM feedback_session_public fs JOIN user u ON fs.user_id = u.user_id JOIN session_public sp ON fs.session_id = sp.session_public_id WHERE fs.session_id = ?';
         pool.query(query, [sessionId], callback);
     },
 }
