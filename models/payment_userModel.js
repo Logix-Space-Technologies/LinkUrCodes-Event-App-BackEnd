97b982b17e3f0a1ec5e578410026d3578c2426e2
Mahadevan1234 <mahi17700@gmail.com>
2024-07-24 18:13:56 +0530

    view user payment history by admin

--- models/payment_userModel.js
+++ models/payment_userModel.js
@@ -57,8 +57,8 @@ const paymentModel = {
   },
 
   viewUserPaymentHistory: (callback) => {
-    const query = 'SELECT p.payment_user_id AS paymentId,u.user_name AS User,e.event_public_name AS Event, e.event_public_description AS EventDescription,p.payment_amount AS Amount,p.order_id AS Invoice, p.payment_date AS Date FROM payment_user p INNER JOIN user u ON p.user_id = u.user_id INNER JOIN event_public e ON p.payment_event_id = e.event_public_id';
-    pool.query(query,callback)
+    const query = 'SELECT p.payment_user_id, p.user_id,u.user_name,u.user_email,u.user_contact_no, p.payment_event_id, e.event_public_name, p.payment_amount,p.payment_date, p.order_id, p.currency, p.status, p.created_at, p.updated_at,p.razorpay_signature, p.notes FROM payment_user p JOIN user u ON p.user_id = u.user_id JOIN event_public e ON p.payment_event_id = e.event_public_id ORDER BY p.payment_user_id DESC';
+    pool.query(query, callback)
   }
 
 }
